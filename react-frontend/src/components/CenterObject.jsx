dde6096b21c2c42a5c3f44858be7bb3a066de951
Peter Kang <pxk7062@rit.edu>
2023-08-30 22:12:53 -0400

    Added Draggable library

--- react-frontend/src/components/CenterObject.jsx
+++ react-frontend/src/components/CenterObject.jsx
@@ -1,14 +1,22 @@
-import React from "react";
 import "./Assets/CenterObject.css";
+import Draggable from "react-draggable";
+import { useRef, useState, React } from "react";
 
 export const CenterObject = (props) => {
   let initClass = "";
   if (props.shapeList?.length > 0) {
     initClass = props.shapeList[0];
   }
+  const [position, setPostiion] = useState({ x: 0, y: 0 });
+  const trackPos = (data) => {
+    setPostiion({ x: data.x, y: data.y });
+    console.log(position);
+  };
+
+  const nodeRef = useRef(null); // for getting Draggable
   return (
-    <div id="centerObject" className={initClass}>
-      <div id="centerObjectHeader"></div>
-    </div>
+    <Draggable nodeRef={nodeRef} onDrag={(e, data) => trackPos(data)}>
+      <div ref={nodeRef} id="centerObject" className={initClass}></div>
+    </Draggable>
   );
 };
